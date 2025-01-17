d899c33970b45e18705954c434321e9d24c0ad90
ljcui1 <yangyangjiaocui@gmail.com>
2025-01-17 14:13:55 -0800

    adjusting input manager (controller movement input does not work)

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -8,7 +8,8 @@ public class PlayerManager : MonoBehaviour
     public PlayerScript Player;
     private Rigidbody2D rb;
 
-    float speedX, speedY = 0;
+    //float speedX, speedY = 0;
+    private Vector2 moveInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +20,44 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        /*Debug.Log("Vertical" + Input.GetAxis("Vertical"));
         // movement
         speedX = 0f;
         speedY = 0f;
 
-        if (Input.GetAxis("Horizontal") != 0)
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            speedX = Input.GetAxis("Horizontal") * Player.moveSpeed;
+            speedX = Input.GetAxisRaw("Horizontal") * Player.moveSpeed;
         }
-        if (Input.GetAxis("Vertical") != 0)
+        if (Input.GetAxisRaw("Vertical") != 0)
         {
-            speedY = Input.GetAxis("Vertical") * Player.moveSpeed;
+            speedY = Input.GetAxisRaw("Vertical") * Player.moveSpeed;
         }
 
         Debug.Log("speedX" + speedX + ", speedY" + speedY);
         rb.velocity = new Vector2(speedX, speedY).normalized * Player.moveSpeed;
+        */
+
+        //movement 2
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
 
         //flip sprite
-        if (speedX > 0)
+        if (moveInput.x > 0)
         {
             Player.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
-        else if (speedX < 0)
+        else if (moveInput.x < 0)
         {
             Player.transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveInput * Player.moveSpeed;
     }
 }
