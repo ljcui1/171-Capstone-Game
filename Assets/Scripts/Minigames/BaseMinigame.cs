4364d49ac10f49e2dfa96ad658d9469a0c6d48e4
kvanarsd <kvanarsd@ucsc.edu>
2025-02-06 15:51:56 -0800

    Create minigame files

--- Assets/Scripts/Minigames/BaseMinigame.cs
+++ Assets/Scripts/Minigames/BaseMinigame.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BaseMinigame : MonoBehaviour
 {
     [SerializeField] private int maxScore;
+    private int curScore = 0;
     [SerializeField] private Attribute attribute;
 
     private MinigameManager manager = MinigameManager.instance;
@@ -21,5 +22,20 @@ public class BaseMinigame : MonoBehaviour
 
     }
 
-
+    private void GameOver()
+    {
+        manager.GameScore(curScore, maxScore, attribute);
+        curScore = 0;
+    }
+    /*
+        // function is referenced from https://discussions.unity.com/t/waitforseconds-while-time-scale-0/552937
+        private IEnumerator WaitForRealSecond(float seconds)
+        {
+            float startTime = Time.realtimeSinceStartup;
+            while (Time.realtimeSinceStartup - startTime < seconds)
+            {
+                yield return null;
+            }
+        }
+    */
 }
