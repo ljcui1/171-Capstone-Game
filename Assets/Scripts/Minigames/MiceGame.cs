74c96b3b4b2768867ac34932b1ab1d171dcfbf7a
ljcui1 <yangyangjiaocui@gmail.com>
2025-02-09 14:30:36 -0800

    moving mouse to mousegame

--- Assets/Scripts/Minigames/MiceGame.cs
+++ Assets/Scripts/Minigames/MiceGame.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiceGame : BaseMinigame
 {
+
+    public Canvas mouseGame;
+
+    [SerializeField]
+    private Image pawa;
+
+    [SerializeField]
+    private Image paws;
+
+    [SerializeField]
+    private Image pawd;
+
+    [SerializeField]
+    private Image pawf;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +30,44 @@ public class MiceGame : BaseMinigame
     {
 
     }
+
+
+
+    public void MouseMiniGamePlay()
+    {
+        mouseGame.enabled = true;
+        /*pawa.enabled = false;
+        paws.enabled = false;
+        pawd.enabled = false;
+        pawf.enabled = false;*/
+        if (Input.GetKey(KeyCode.A))
+        {
+            pawa.enabled = true;
+            paws.enabled = false;
+            pawd.enabled = false;
+            pawf.enabled = false;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            pawa.enabled = false;
+            paws.enabled = true;
+            pawd.enabled = false;
+            pawf.enabled = false;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            pawa.enabled = false;
+            paws.enabled = false;
+            pawd.enabled = true;
+            pawf.enabled = false;
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            pawa.enabled = false;
+            paws.enabled = false;
+            pawd.enabled = false;
+            pawf.enabled = true;
+        }
+    }
 }
+
--- Assets/Scripts/Minigames/MinigameManager.cs
+++ Assets/Scripts/Minigames/MinigameManager.cs
@@ -6,19 +6,6 @@ using UnityEngine.UI;
 public class MinigameManager : MonoBehaviour
 {
     public static MinigameManager instance { get; private set; }
-    public Canvas mouseGame;
-
-    [SerializeField]
-    private Image pawa;
-
-    [SerializeField]
-    private Image paws;
-
-    [SerializeField]
-    private Image pawd;
-
-    [SerializeField]
-    private Image pawf;
 
     [Header("Customer")]
     [SerializeField] private CustomerManager CustomerMan;
@@ -42,7 +29,7 @@ public class MinigameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mouseGame.enabled = false;
+
     }
 
     // Update is called once per frame
@@ -57,43 +44,6 @@ public class MinigameManager : MonoBehaviour
                 CustomerMan.AddCustomerProbability(customersToAdd[i], customersToAdd[i] / 100, attribute);
                 return;
             }
-        }
-    }
-
-    public void MouseMiniGamePlay()
-    {
-        mouseGame.enabled = true;
-        /*pawa.enabled = false;
-        paws.enabled = false;
-        pawd.enabled = false;
-        pawf.enabled = false;*/
-        if (Input.GetKey(KeyCode.A))
-        {
-            pawa.enabled = true;
-            paws.enabled = false;
-            pawd.enabled = false;
-            pawf.enabled = false;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            pawa.enabled = false;
-            paws.enabled = true;
-            pawd.enabled = false;
-            pawf.enabled = false;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            pawa.enabled = false;
-            paws.enabled = false;
-            pawd.enabled = true;
-            pawf.enabled = false;
-        }
-        else if (Input.GetKey(KeyCode.F))
-        {
-            pawa.enabled = false;
-            paws.enabled = false;
-            pawd.enabled = false;
-            pawf.enabled = true;
         }
     }
 }
